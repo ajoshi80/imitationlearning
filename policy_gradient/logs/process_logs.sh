07e4c212a4512388450a6606fd21d13bc935c72f
Anirudh Joshi <ajoshi80@stanford.edu>
2020-03-22 17:08:42 -0700

    Deleted Policy Gradients

--- policy_gradient/logs/process_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for log in *.log; do
-    grep 'Reward total' "$log" \
-        | awk '{print $4, $11}' \
-        | sed 's/\;//g' \
-        | awk '{print NR " " $0}' \
-        > "${log%.log}.dat"
-done
